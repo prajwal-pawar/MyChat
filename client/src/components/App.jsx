55427da44e7de3aa62eff908c9ea778d88bc65c5
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-07-12 23:44:27 +0530

    add tailwindcss & daisyui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return "app";
+  return (
+    <div>
+      <button className="btn">click me</button>
+      <button className="btn btn-neutral">click me</button>
+      <button className="btn btn-primary">click me</button>
+    </div>
+  );
 }
 
 export default App;
