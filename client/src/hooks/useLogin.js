b3de0bebba0476611aae9c8bb832135e7504f7c5
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-07-16 00:14:10 +0530

    add login

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import useAuth from "./useAuth";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { setAuthUser } = useAuth();
+
+  const login = async (username, password) => {
+    setLoading(true);
+
+    try {
+      // if input fields are empty
+      if (!username || !password) {
+        toast.error("please fill in all fields");
+        return;
+      }
+
+      // hit login api endpoint
+      const response = await axios.post("http://localhost:8000/auth/login", {
+        username,
+        password,
+      });
+
+      console.log(response.data);
+
+      // if error
+      if (response.data.error) {
+        throw new Error(response.data.error);
+      }
+
+      // save user to local storage
+      localStorage.setItem("user", JSON.stringify(response.data));
+      // set user in context
+      setAuthUser(response.data);
+    } catch (err) {
+      toast.error(err.response.data.error);
+      console.log(err.response.data.error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, login };
+};
+
+export default useLogin;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // login hook
+  const { loading, login } = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    await login(username, password);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-1/2 p-6 rounded-lg bg-gray-700">
@@ -8,23 +22,39 @@ const Login = () => {
           Login <span className="text-blue-500"> MyChat</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="mt-2">
             <label className="input input-bordered flex items-center gap-5">
               Username
-              <input type="text" className="grow text-white" />
+              <input
+                type="text"
+                className="grow text-white"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </label>
           </div>
 
           <div className="mt-2">
             <label className="input input-bordered flex items-center gap-5">
               Password
-              <input type="password" className="grow text-white" />
+              <input
+                type="password"
+                className="grow text-white"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </label>
           </div>
 
           <div>
-            <button className="btn btn-block btn-md mt-2">Login</button>
+            <button className="btn btn-block btn-md mt-2" disabled={loading}>
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
 
           <hr className="mt-5" />
